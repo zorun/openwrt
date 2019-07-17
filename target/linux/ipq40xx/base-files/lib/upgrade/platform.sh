512365629a33330eb59aed09a0580f72a8cef6f4
Rafał Miłecki <rafal@milecki.pl>
2019-07-17 11:32:54 +0200

    ipq40xx: sysupgrade: get rid of platform_nand_pre_upgrade()
    1) nand_do_upgrade() is always called by a target code
    2) nand_do_upgrade() starts with calling platform_nand_pre_upgrade()
    
    It means there is no need for the platform_nand_pre_upgrade() callback
    at all. All code that was present there could bo moved & simplly called
    by a target right before the nand_do_upgrade().
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    (cherry picked from commit 1cbe0d659c4438cc6348093385a24e969aeadac0)
    
    [Only apply to ipq40xx]

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -47,11 +47,21 @@ zyxel_do_upgrade() {
 platform_do_upgrade() {
 	case "$(board_name)" in
 	8dev,jalapeno |\
-	alfa-network,ap120c-ac |\
 	avm,fritzbox-7530 |\
 	avm,fritzrepeater-1200 |\
 	avm,fritzrepeater-3000 |\
 	qxwlan,e2600ac-c2)
+		nand_do_upgrade "$1"
+		;;
+	alfa-network,ap120c-ac)
+		part="$(awk -F 'ubi.mtd=' '{printf $2}' /proc/cmdline | sed -e 's/ .*$//')"
+		if [ "$part" = "rootfs1" ]; then
+			fw_setenv active 2 || exit 1
+			CI_UBIPART="rootfs2"
+		else
+			fw_setenv active 1 || exit 1
+			CI_UBIPART="rootfs1"
+		fi
 		nand_do_upgrade "$1"
 		;;
 	asus,map-ac2200)
@@ -81,28 +91,6 @@ platform_do_upgrade() {
 		;;
 	*)
 		default_do_upgrade "$1"
-		;;
-	esac
-}
-
-platform_nand_pre_upgrade() {
-	case "$(board_name)" in
-	alfa-network,ap120c-ac)
-		part="$(awk -F 'ubi.mtd=' '{printf $2}' /proc/cmdline | sed -e 's/ .*$//')"
-		if [ "$part" = "rootfs1" ]; then
-			fw_setenv active 2 || exit 1
-			CI_UBIPART="rootfs2"
-		else
-			fw_setenv active 1 || exit 1
-			CI_UBIPART="rootfs1"
-		fi
-		;;
-	asus,rt-ac58u)
-		CI_UBIPART="UBI_DEV"
-		CI_KERNPART="linux"
-		;;
-	meraki,mr33)
-		CI_KERNPART="part.safe"
 		;;
 	esac
 }
